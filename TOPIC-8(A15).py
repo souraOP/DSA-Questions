ab28d78423ed6014941ec12347396bcd6074ffa1
Sourasish Mondal <sourasishmondal95@gmail.com>
2022-09-07 16:16:20 +0530

    Create TOPIC-8(A15).py

---
+++ TOPIC-8(A15).py
@@ -0,0 +1,13 @@
+def WordCount(search_word):
+    f = open("/home/linuxmint/Desktop/agatha.txt", 'r')
+    count = 0
+    s = f.read()
+    x = s.split()
+    for i in x:
+        if i == search_word:
+            count +=1
+    print("The word is " + str(count)+ " times")
+
+
+search_word = input("Enter the word: ")
+WordCount(search_word)
