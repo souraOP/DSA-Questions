9852817c6a70d2807a0c17e90d2b974e7c745bf7
Sourasish Mondal <sourasishmondal95@gmail.com>
2022-11-15 11:43:08 +0530

    Time: 8 ms (13.72%), Space: 41.3 MB (30.98%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,13 +1,6 @@
 class Solution {
     public int maximum69Number (int num) {
-        StringBuilder superSB = new StringBuilder();
-        superSB.append(num);
-        for(int i = 0; i < superSB.length(); i++){
-            if(superSB.charAt(i) == '6'){
-                superSB.setCharAt(i, '9');
-                break;
-            }
-        }
-        return Integer.parseInt(superSB.toString());
+        String ankita = "" + num;
+        return Integer.parseInt(ankita.replaceFirst("6", "9"));
     }
 }
