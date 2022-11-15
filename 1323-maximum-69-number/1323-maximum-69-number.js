0dc45a07fce71dded6e0d8d1cff14a83f4e31937
Sourasish Mondal <sourasishmondal95@gmail.com>
2022-11-15 11:36:37 +0530

    Time: 77 ms (80.60%), Space: 41.9 MB (53.60%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -1,13 +1,9 @@
-class Solution {
-    public int maximum69Number (int num) {
-        StringBuilder superSB = new StringBuilder();
-        superSB.append(num);
-        for(int i = 0; i < superSB.length(); i++){
-            if(superSB.charAt(i) == '6'){
-                superSB.setCharAt(i, '9');
-                break;
-            }
-        }
-        return Integer.parseInt(superSB.toString());
-    }
-}
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    let etjwk = num.toString();
+    let afff = etjwk.replace('6', '9');
+    return Number(afff);
+};
