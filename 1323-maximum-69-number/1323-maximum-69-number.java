81863190b02d38f51012a094d489270a61dd2515
Sourasish Mondal <sourasishmondal95@gmail.com>
2022-11-15 11:48:39 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (75.16%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,5 +1,14 @@
 class Solution {
     public int maximum69Number (int num) {
-        return Integer.parseInt(("" + num).replaceFirst("6", "9"));
+        if(num%10000 >= 6000 && num%10000 < 9000){
+            num += 3000;
+        } else if(num % 1000 >= 600 && num % 1000 < 900){
+            num += 300;
+        } else if(num % 100 >= 60 && num % 100 < 90){
+            num += 30;
+        } else if(num % 10 == 6){
+            num += 3;
+        }
+        return num;
     }
 }
