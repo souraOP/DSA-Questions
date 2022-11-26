4176e1011c1189b8562b6f1346beca6f85858c5b
Sourasish Mondal <sourasishmondal95@gmail.com>
2022-11-27 03:10:54 +0530

    Added Add Binary Strings Java Code

---
+++ GFG - Add Binary Strings/addBinary.java
@@ -0,0 +1,9 @@
+class Solution {
+    String addBinary(String A, String B) {
+        // code here
+        java.math.BigInteger a = new java.math.BigInteger(A,2);
+        java.math.BigInteger b = new java.math.BigInteger(B,2);
+        a = a.add(b);
+        return a.toString(2);
+    }
+}
