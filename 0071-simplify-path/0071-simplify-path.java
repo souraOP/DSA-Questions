029fee4c186a5b8edb24c2b4113f40587a08cdac
Sourasish Mondal <sourasishmondal95@gmail.com>
2023-04-12 15:48:04 +0530

    Time: 5 ms (52.40%), Space: 42.4 MB (84.49%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,21 +1,18 @@
-class Solution {
-    public String simplifyPath(String path) {
+class Solution{
+    public String simplifyPath(String path){
         Stack<String> s = new Stack<>();
-        StringBuilder res = new StringBuilder();
-        String[] p =path.split("/");
-        
-        for(int i=0;i<p.length;i++){
-            if(!s.isEmpty()  && p[i].equals("..")) s.pop();
+        StringBuilder sb = new StringBuilder();
+        String[] p = path.split("/");
+        for(int i = 0; i < p.length; i++){
+            if(!s.isEmpty() && p[i].equals("..")) s.pop();
             else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
                 s.push(p[i]);
         }
-        
-        
+
         if(s.isEmpty()) return "/";
         while(!s.isEmpty()){
-            res.insert(0,s.pop()).insert(0,"/");
+            sb.insert(0, s.pop()).insert(0, "/");
         }
-        
-        return res.toString();
+        return sb.toString();
     }
 }
