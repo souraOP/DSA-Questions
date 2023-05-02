68b606493ac1f83c4b9f1c9a821685e0f388df70
Sourasish Mondal <sourasishmondal95@gmail.com>
2023-05-02 13:36:41 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (50.18%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,9 +1,14 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int product = 1;
+        int number = 1;  //keeping the number as 1 (positive) because whenever I see any -ve integer i will flip it to '-number' & as soon as another negative integer comes then '-(-number)' i.e, will be positive. So, keeping it +ve!
         for(int i: nums){
-            product *= Math.signum(i);
+            if(i == 0){
+                return 0;
+            }
+            if(i < 0){   //if I come across any negative integer then just flip the sign of the number
+                number = -number;
+            }
         }
-        return product;
+        return number;
     }
 }
