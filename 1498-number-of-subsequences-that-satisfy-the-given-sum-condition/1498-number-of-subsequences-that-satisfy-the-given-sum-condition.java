d839d4f3765f9b317408a4a7e0b2909f85cd2f5f
Sourasish Mondal <sourasishmondal95@gmail.com>
2023-05-06 23:04:16 +0530

    Time: 33 ms (100.00%), Space: 51.1 MB (68.24%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,18 +1,24 @@
 class Solution {
-    public int numSubseq(int[] A, int target) {
-        Arrays.sort(A);
-        int res = 0, n = A.length, l = 0, r = n - 1, mod = (int)1e9 + 7;
-        int[] pows = new int[n];
-        pows[0] = 1;
-        for (int i = 1 ; i < n ; ++i)
-            pows[i] = pows[i - 1] * 2 % mod;
-        while (l <= r) {
-            if (A[l] + A[r] > target) {
-                r--;
-            } else {
-                res = (res + pows[r - l++]) % mod;
-            }
-        }
-        return res;
+  public int numSubseq(int[] nums, int target) {
+    var n = nums.length;
+    Arrays.sort(nums);
+
+    var exp = new int[n];
+    exp[0] = 1;
+
+    for (var i=1; i<n; i++)
+      exp[i] = (exp[i-1] * 2) % 1000000007;
+
+    int i = 0, j = n-1, cnt = 0;
+
+    while (i <= j) {
+      if (nums[i] + nums[j] <= target) {
+        cnt = (cnt + exp[j-i]) % 1000000007;
+        i++;
+      } else {
+        j--;
+      }
     }
+    return cnt;
+  }
 }
