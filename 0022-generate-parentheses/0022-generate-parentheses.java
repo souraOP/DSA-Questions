ceab3e35b1d16083c8352cd04259f033b294a986
Sourasish Mondal <sourasishmondal95@gmail.com>
2023-08-20 22:14:33 +0530

    Time: 1 ms (93.31%), Space: 43.3 MB (63.17%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int n){
+        if(str.length() == n * 2){
+            list.add(str);
+            return;
+        }
+        if(open < n){
+            backtrack(list, str+"(", open+1, close, n);
+        }
+        if(close < open){
+            backtrack(list, str+")", open, close+1, n);
+        }
+    }
+}
