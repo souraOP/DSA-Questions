f9548877619980cbfe95bbcc7723f0f2473602ad
Sourasish Mondal <sourasishmondal95@gmail.com>
2023-10-06 07:00:41 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (49.75%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,7 +1,28 @@
 class Solution {
     public String interpret(String command) {
-        String tS = command.replace("(al)", "al");
-        String fS = tS.replace("()", "o");
-        return fS;
+        //1st Solution
+        // String tS = command.replace("(al)", "al");
+        // String fS = tS.replace("()", "o");
+        // return fS;
+        
+        //return command.replace("()", "o").replace("(al)", "al");
+        
+        //2nd Solution
+        StringBuilder sb = new StringBuilder(command.length());
+        for(int i = 0; i < command.length(); i++){
+            if(command.charAt(i) == 'G'){
+                sb.append('G');
+            }
+            if (command.charAt(i) == '('){
+                if(command.charAt(i + 1) == ')'){
+                    sb.append('o');
+                    i= i + 1;
+                } else {
+                    sb.append("al");
+                    i = i + 3;
+                }
+            }
+        }
+        return sb.toString();
     }
 }
