998a89b7b3ac330a3eb198e16c62de3cd13023cb
Sourasish Mondal <sourasishmondal95@gmail.com>
2023-10-06 07:01:10 +0530

    Time: 1 ms (65.48%), Space: 40.4 MB (63.79%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -5,24 +5,24 @@ class Solution {
         // String fS = tS.replace("()", "o");
         // return fS;
         
-        //return command.replace("()", "o").replace("(al)", "al");
+        return command.replace("()", "o").replace("(al)", "al");
         
         //2nd Solution
-        StringBuilder sb = new StringBuilder(command.length());
-        for(int i = 0; i < command.length(); i++){
-            if(command.charAt(i) == 'G'){
-                sb.append('G');
-            }
-            if (command.charAt(i) == '('){
-                if(command.charAt(i + 1) == ')'){
-                    sb.append('o');
-                    i= i + 1;
-                } else {
-                    sb.append("al");
-                    i = i + 3;
-                }
-            }
-        }
-        return sb.toString();
+        // StringBuilder sb = new StringBuilder(command.length());
+        // for(int i = 0; i < command.length(); i++){
+        //     if(command.charAt(i) == 'G'){
+        //         sb.append('G');
+        //     }
+        //     if (command.charAt(i) == '('){
+        //         if(command.charAt(i + 1) == ')'){
+        //             sb.append('o');
+        //             i= i + 1;
+        //         } else {
+        //             sb.append("al");
+        //             i = i + 3;
+        //         }
+        //     }
+        // }
+        // return sb.toString();
     }
 }
