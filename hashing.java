cf1637b2a1ed9e7b7301d7ce8b8b7b4046e9f5bc
Sourasish Mondal <sourasishmondal95@gmail.com>
2024-04-07 16:36:05 +0530

    Add files via upload

--- hashing.java
+++ hashing.java
@@ -3,25 +3,25 @@ import java.util.*;
 class hashing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
-        }
+        // int n = sc.nextInt();
+        // int[] arr = new int[n];
+        // for(int i = 0; i < n; i++){
+        //     arr[i] = sc.nextInt();
+        // }
 
-        // precompute the hash table
-        int[] hash = new int[13];
-        for(int i = 0; i < n; i++) {
-            hash[arr[i]] += 1;
-        }
+        // // precompute the hash table
+        // int[] hash = new int[13];
+        // for(int i = 0; i < n; i++) {
+        //     hash[arr[i]] += 1;
+        // }
 
-        int totalQueries = sc.nextInt();
-        while(totalQueries-- > 0) {
-            int number;
-            number = sc.nextInt();
-            // fetching the value from the hash table
-            System.out.println(hash[number]);
-        }
+        // int totalQueries = sc.nextInt();
+        // while(totalQueries-- > 0) {
+        //     int number;
+        //     number = sc.nextInt();
+        //     // fetching the value from the hash table
+        //     System.out.println(hash[number]);
+        // }
 
         // for character hashing
 
@@ -42,6 +42,48 @@ class hashing {
 
         // For array with Uppercase Characters and also for lowerCase Characters, create an array of 256 size and use (Char - 'A') for hashing
 
+        // String s = sc.next();
 
+        // int[] hash = new int[26];
+        // // precompute the hash array
+        // for(int i = 0; i < s.length(); i++){
+        //     hash[s.charAt(i) - 'a']++;
+        // }
+        // int queries = sc.nextInt();
+        // while(queries-- > 0) {
+        //     char c = sc.next().charAt(0);
+        //     // fetch
+        //     System.out.println(hash[c - 'a']);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        //precompute
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            if(mpp.containsKey(arr[i])){
+                mpp.put(arr[i], mpp.get(arr[i]) + 1);
+            } else {
+                mpp.put(arr[i], 1);
+            }
+        }
+
+        // iterate over the hash map
+        // for(HashMap.Entry<Integer, Integer> it: mpp.entrySet()){
+        //     System.out.println(it.getKey() + "->" + it.getValue());
+        // }
+        System.out.println(mpp.entrySet());
+
+        int queries = sc.nextInt();
+        while(queries-- > 0) {
+            int number = sc.nextInt();
+            //fetch
+            if(mpp.containsKey(number)) System.out.println(mpp.get(number));
+            else System.out.println(0);
+        }
+
+        // for Character hashing using HashMap: HashMap<Character, Integer>
     }
 }
