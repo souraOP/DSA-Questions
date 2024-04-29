dd043efc01cb709f7115e11ea8a7d619fc5658ed
Sourasish Mondal <sourasishmondal95@gmail.com>
2024-04-29 07:49:42 +0530

    Time: 14 ms (15.09%), Space: 44.8 MB (93.54%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,36 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            int c = 0;
-            // store the element
-            int z = nums[i];
+//         for(int i = 0; i < nums.length; i++) {
+//             int c = 0;
+//             // store the element
+//             int z = nums[i];
             
-            // apply linear search
-            for(int j = 0; j < nums.length; j++) {
-                if(nums[j] == z){
-                    c++;
-                }
-            }
-            if(c == 1) return z;
+//             // apply linear search
+//             for(int j = 0; j < nums.length; j++) {
+//                 if(nums[j] == z){
+//                     c++;
+//                 }
+//             }
+//             if(c == 1) return z;
+//         }
+//         return -1;
+        
+        // Using hashmap
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        //putting everything inside the hashmap
+        for(int i = 0; i < nums.length; i++){
+            //loop through and put the element alongwith their frequency
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
+        for(Map.Entry<Integer, Integer> it: map.entrySet()) {
+            if(it.getValue() == 1) {
+                return it.getKey();
+            }
+        }
+        
         return -1;
+        
+        
     }
 }
