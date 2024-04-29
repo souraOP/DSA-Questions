3cd807b3f4753f71b602a7ae3ca7b02467f0b6fc
Sourasish Mondal <sourasishmondal95@gmail.com>
2024-04-29 07:40:25 +0530

    Time: 354 ms (7.23%), Space: 46.6 MB (8.57%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ankita = 0;
-        for(int fuck: nums){
-            ankita = ankita ^ fuck;
+        for(int i = 0; i < nums.length; i++) {
+            int c = 0;
+            // store the element
+            int z = nums[i];
             
+            // apply linear search
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[j] == z){
+                    c++;
+                }
+            }
+            if(c == 1) return z;
         }
-        return ankita;
-        
+        return -1;
     }
 }
