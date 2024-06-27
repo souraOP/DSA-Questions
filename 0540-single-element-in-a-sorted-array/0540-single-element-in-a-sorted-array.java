8a7d342c21b946df4351ed7aae7d0ee69cc47bbc
Sourasish Mondal <sourasishmondal95@gmail.com>
2024-06-27 22:39:16 +0530

    Time: 0 ms (100.00%), Space: 49.7 MB (95.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,15 +1,35 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         // using hashmap
-        HashMap<Integer, Integer> singleElement = new HashMap<>();
-        for(int i : nums){
-            singleElement.put(i, singleElement.getOrDefault(i, 0) + 1);
-        }
-        for(Map.Entry<Integer, Integer> entry : singleElement.entrySet()) {
-            if(entry.getValue() == 1) {
-                return entry.getKey();
+        // HashMap<Integer, Integer> singleElement = new HashMap<>();
+        // for(int i : nums){
+        //     singleElement.put(i, singleElement.getOrDefault(i, 0) + 1);
+        // }
+        // for(Map.Entry<Integer, Integer> entry : singleElement.entrySet()) {
+        //     if(entry.getValue() == 1) {
+        //         return entry.getKey();
+        //     }
+        // }
+        // return -1;
+        
+        // using binary search
+        int low = 0;
+        int high = nums.length - 1;
+        while(low < high) {
+            int mid = low + (high - low) /2;
+            if(mid % 2 == 1) {
+                mid--;
             }
+            
+            if(nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            }
+            else {
+                high = mid;
+            }
+            
+            
         }
-        return -1;
+        return nums[low];
     }
 }
