6b9217013d7580c0765c77464981e6cf5d80ef9d
Sourasish Mondal <sourasishmondal95@gmail.com>
2024-07-04 19:10:47 +0530

    Time: 5 ms (99.92%), Space: 45.6 MB (85.01%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,10 +1,27 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j] == target) {
-                    return true;
-                }
+        // for(int i = 0; i < matrix.length; i++) {
+        //     for(int j = 0; j < matrix[i].length; j++) {
+        //         if(matrix[i][j] == target) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        if(matrix.length == 0 || matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+            return false;
+        }
+        
+        int col = matrix[0].length - 1;
+        int row = 0;
+        while(col >= 0 && row <= matrix.length - 1) {
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if (target < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
             }
         }
         return false;
