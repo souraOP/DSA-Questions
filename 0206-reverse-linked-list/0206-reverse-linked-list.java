6776430a76ca34570a6be0cf48611e9213e59da2
Sourasish Mondal <sourasishmondal95@gmail.com>
2024-07-28 15:50:42 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (58.46%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,15 +10,40 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode newHead = null;
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = next;
+        if(head == null) return null;
+        if(head.next == null){
+            return head;
         }
-        return newHead;
-
+//         Stack<Integer> st = new Stack<>();
+//         ListNode temp = head;
+//         // push all the nodes into the stack
+//         while(temp != null){
+//             st.push(temp.val);
+//             temp = temp.next;
+//         }
+        
+//         // after pushing every node into the stack
+//         // pop from the stack so that they come out as reverse order
+//         temp = head;
+//         while(temp != null){
+//             temp.val = st.pop();  // assigning the original linkedlist value as the stack pop value.
+//             temp = temp.next;
+//         }
+//         // so that when we return head the linked list will be in reverse order
+//         return head;
+        
+        //optimal approach
+        // here we are reversing the nodes one by one as we traverse the linkedlist 
+        
+        ListNode temp = head;
+        ListNode prev = null;
+        
+        while(temp != null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev= temp;
+            temp= front;
+        }
+        return prev;
     }
-    
 }
