e40a3e248a5a65a6869f9450e51d9a395baff7cf
Sourasish Mondal <sourasishmondal95@gmail.com>
2025-01-22 14:41:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Infix to Postfix/infix-to-postfix.java
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.java
@@ -19,53 +19,58 @@ class GFG {
 
 
 class Solution {
-    public static int rank(char ch){
-        if(ch == '+' || ch == '-') return 1;
-        if(ch == '/' || ch == '*') return 2;
-        if(ch == '^') return 3;
-        
-        return -1;
-    }
-    public static boolean checkIsOperator(char ch){
-        if(ch == '+' || ch == '-' ||
-        ch == '*' || ch == '/' || ch == '^' ||
-        ch == '(' || ch == ')') return true;
-        
-        return false;
-    }
     // Function to convert an infix expression to a postfix expression.
     public static String infixToPostfix(String s) {
-        StringBuilder ans = new StringBuilder();
-        Stack<Character> stk = new Stack();
-        
-        int len = s.length();
-        
-        for(int i=0; i<len; i++){
-            char ch = s.charAt(i);
-                // System.out.println(ans+"\t"+ch);
-            if(!checkIsOperator(ch)){     // if ch is not an operator
-                ans.append(ch);
-            }
-            else{               // if ch is an operator
-                if(stk.isEmpty() || ch == '(') stk.push(ch);    // if stk is empty or a open bracket occurs
-                else if(ch == ')'){
-
-                    while(!stk.isEmpty() && stk.peek() != '('){
-                        ans.append(stk.pop());
+        // Your code here
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            // if character is an operand
+            if(!checkIfOperator(c)){
+                sb.append(c);
+            // if character is opening braces
+            } else{
+                 if(c == '(' || st.isEmpty()){
+                    st.push(c);
+                // if character is closing braces
+                } else if (c == ')'){
+                    while(!st.isEmpty() && st.peek() != '('){
+                        sb.append(st.pop()); // put everything back to ans and pop from stack
                     }
-                    stk.pop();
-                }
-                else{       // if stack is not empty
-                    while(!stk.isEmpty() && rank(stk.peek()) >= rank(ch)){  // check stk peek element's precedence should be less than current ch's precedence
-                        ans.append(stk.pop());
+                    st.pop(); // pop the last opening bracket also
+                } else {
+                    while(!st.isEmpty() && rankOrder(st.peek()) >= rankOrder(c)){
+                        sb.append(st.pop());
                     }
-                    
-                    stk.push(ch);
+                    st.push(c);
                 }
             }
         }
-        while(!stk.isEmpty()) ans.append(stk.pop());
-        
-        return ans.toString();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.toString();
+    }
+    
+    static int rankOrder(char c){
+        if(c == '^'){
+            return 3;
+        }
+        if (c == '*' || c == '/'){
+            return 2;
+        }
+        if (c == '+' || c == '-'){
+            return 1;
+        }
+        return -1;
+    }
+    
+    static boolean checkIfOperator(char c){
+        if(c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '(' || c == ')'){
+            return true;
+        }
+        return false;
     }
 }
